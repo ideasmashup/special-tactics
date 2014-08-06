4d8a043583f83d95b2a6b57ad84dccb1ac30557c
William ANGER <anger.william@gmail.com>
2014-08-06 17:52:21 +0200

    [wip] experimental mineral patch agent

--- SpecialTactics/src/org/ideasmashup/specialtactics/agents/MineralPatch.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/agents/MineralPatch.java
@@ -1,17 +1,45 @@
 package org.ideasmashup.specialtactics.agents;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ideasmashup.specialtactics.needs.Need;
+import org.ideasmashup.specialtactics.needs.NeedServant;
+import org.ideasmashup.specialtactics.utils.UType;
+import org.ideasmashup.specialtactics.utils.Utils;
+
 import bwapi.Unit;
 
 public class MineralPatch {
 
-	protected Unit bindee;
-	
+	protected Unit bindee; // the mineral patch
+	protected List<Unit> servants; // the workers assigned to this patch
+
 	public MineralPatch() {
-		// TODO Auto-generated constructor stub
+		this.servants = new ArrayList<Unit>(3);
 	}
-	
+
 	public void update() {
-		
+		// check if the
+		if (bindee.getResources() < 10) {
+
+		}
+	}
+
+	public Need[] getNeeds() {
+		return new Need[]{new NeedServant(Utils.get().getTypeFor(UType.WORKER))};
+	}
+
+	public void addServant(Unit unit) {
+
+	}
+
+	public void freeServant(Unit unit) {
+
+	}
+
+	public void freeAllServants() {
+
 	}
 
 }
