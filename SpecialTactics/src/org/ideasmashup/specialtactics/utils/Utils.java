487a0ce37cdc88bc5429e18a970d91a92e2d8a0c
William ANGER <anger.william@gmail.com>
2014-08-06 17:49:55 +0200

    singleton utility class for agents and other lightweight objects to be able to fetch game and AI instances references when they require "game-state or higher-level awareness"

---
+++ SpecialTactics/src/org/ideasmashup/specialtactics/utils/Utils.java
@@ -0,0 +1,43 @@
+package org.ideasmashup.specialtactics.utils;
+
+import bwapi.Game;
+import bwapi.Player;
+import bwapi.UnitType;
+
+public class Utils {
+
+	private static Utils instance = null;
+
+	private final Player player;
+	private final Game game;
+
+	protected Utils(Game game) {
+		this.game = game;
+		this.player = game.self();
+	}
+
+	public static Utils init(Game game) {
+		if (instance == null) {
+			instance = new Utils(game);
+		}
+
+		return instance;
+	}
+
+	public static Utils get() {
+		return instance;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public UnitType getTypeFor(UType type){
+		return type.get(getPlayer().getRace());
+	}
+
+}
