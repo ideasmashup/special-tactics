fcd3761db9007ddc04cb12baa2ebfd4e209fc010
William ANGER <anger.william@gmail.com>
2014-08-06 16:49:42 +0200

    [wip] binding one MineralPatch agent per mineral patch at startup

--- SpecialTactics/src/org/ideasmashup/specialtactics/brains/Brain.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/brains/Brain.java
@@ -1,5 +1,10 @@
 package org.ideasmashup.specialtactics.brains;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.ideasmashup.specialtactics.agents.Bindable;
+
 import bwapi.DefaultBWListener;
 import bwapi.Game;
 import bwapi.Mirror;
@@ -13,8 +18,12 @@ public class Brain extends DefaultBWListener {
 	protected Game game;
 	protected Player self;
 	
+	protected List<Bindable> agents;
+	
 	public Brain(Mirror mirror) {
 		this.mirror = mirror;
+		
+		agents = new ArrayList<Bindable>();
 	}
 	
 	public Brain(Game game) {
@@ -38,6 +47,19 @@ public class Brain extends DefaultBWListener {
 		BWTA.analyze();
 		System.out.println("Map data ready");
 		*/
+		
+		// Initialize all constraints and agents
+		System.out.println("Starting by listing all visible units ");
+		
+		for (Unit unit : self.getUnits()) {
+			System.out.println("Found "+ unit.getType());
+			 
+			//
+			if (unit.getType() == UnitType.Resource_Mineral_Field) {
+				System.out.println("  - is mineral patch : assigned MineralPatch agent");
+				
+			}
+		}
 	}
 	
 	@Override
