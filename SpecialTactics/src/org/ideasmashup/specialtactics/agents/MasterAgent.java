ed916ac5143335cad2b8594d2659c0c84ecbcf70
William ANGER <anger.william@gmail.com>
2014-08-08 18:39:19 +0200

    new master agent class for all agents that control "slave/servants" units
    master agents examples : mineral patches & geysers (control their own haversters), spy agent (control a scouting unit), squad agent (control units assigned to itself)
    
    "to control" here means micro-management of every moves of the servants units, when the agent is destroyed it will "free" its servants so that other agents can take them over

---
+++ SpecialTactics/src/org/ideasmashup/specialtactics/agents/MasterAgent.java
@@ -0,0 +1,28 @@
+package org.ideasmashup.specialtactics.agents;
+
+import java.util.List;
+
+import bwapi.Unit;
+
+public class MasterAgent extends Agent {
+
+	protected List<Unit> servants;
+
+	public MasterAgent(Unit bindee) {
+		super(bindee);
+	}
+
+	protected void addServant(Unit unit) {
+	
+	}
+
+	protected void freeServant(Unit unit) {
+		servants.remove(unit);
+		// Utils.getProductChannels().add(unit);
+	}
+
+	protected void freeAllServants() {
+	
+	}
+
+}
