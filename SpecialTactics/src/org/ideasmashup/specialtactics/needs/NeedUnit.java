46c395670faaaa1442653ce6b27e6e1cd1d35a2b
William ANGER <anger.william@gmail.com>
2014-08-11 07:39:38 +0200

    renamed NeedServant as NeedUnit

--- SpecialTactics/src/org/ideasmashup/specialtactics/needs/NeedUnit.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/needs/NeedUnit.java
@@ -2,16 +2,16 @@ package org.ideasmashup.specialtactics.needs;
 
 import bwapi.UnitType;
 
-public class NeedServant extends Need {
+public class NeedUnit extends Need {
 
 	protected UnitType type;
 
-	public NeedServant(UnitType type) {
+	public NeedUnit(UnitType type) {
 		super();
 		this.type = type;
 	}
 
-	public NeedServant(UnitType type, float priority) {
+	public NeedUnit(UnitType type, float priority) {
 		super(priority);
 		this.type = type;
 	}
