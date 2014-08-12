b01062f4959deb8632828565c887f7cbc93ca0b8
William ANGER <anger.william@gmail.com>
2014-08-12 02:13:16 +0200

    new Need types for supply

---
+++ SpecialTactics/src/org/ideasmashup/specialtactics/needs/NeedSupply.java
@@ -0,0 +1,34 @@
+package org.ideasmashup.specialtactics.needs;
+
+import org.ideasmashup.specialtactics.needs.Needs.Types;
+
+public class NeedSupply extends Need {
+
+	protected int supply;
+	protected final Needs.Types[] types;
+
+	public NeedSupply(int supply) {
+		super();
+		this.types = new Types[]{Needs.Types.SUPPLY};
+		this.supply = supply;
+	}
+
+	public NeedSupply(int supply, float priority) {
+		super(priority);
+		this.types = new Types[]{Needs.Types.SUPPLY};
+		this.supply = supply;
+	}
+
+	@Override
+	public Types[] getTypes() {
+		return types;
+	}
+
+	@Override
+	public boolean canReceive(Object offer) {
+		// accept all offers because current supply value is
+		// fetched directly from the Player instance so the "offer'
+		// is ignored (and generally "null" except for special cases)
+		return true;
+	}
+}
