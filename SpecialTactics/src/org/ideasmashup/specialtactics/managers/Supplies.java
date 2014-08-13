1c80393825198ed72d5ab57c5fea1b103b03cb04
William ANGER <anger.william@gmail.com>
2014-08-13 20:01:32 +0200

    Supplies now request a WORKER to be able to create new supply provider (building, no support yet for zerg)

--- SpecialTactics/src/org/ideasmashup/specialtactics/managers/Supplies.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/managers/Supplies.java
@@ -3,9 +3,15 @@ package org.ideasmashup.specialtactics.managers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.ideasmashup.specialtactics.agents.Consumer;
 import org.ideasmashup.specialtactics.listeners.SupplyListener;
+import org.ideasmashup.specialtactics.needs.Need;
+import org.ideasmashup.specialtactics.needs.NeedUnit;
 
-public class Supplies {
+import bwapi.Unit;
+import bwapi.UnitType;
+
+public class Supplies implements Consumer {
 
 	protected List<SupplyListener> listeners;
 
@@ -36,9 +42,33 @@ public class Supplies {
 	}
 
 	public static void onSupplyChange(int supply) {
+		// automatically create supply
+		if (supply <= 3) {
+			Needs.add(new NeedUnit(Units.Types.WORKERS.getUnitType(), 0), instance);
+		}
+
 		// call all listeners
 		for (SupplyListener ls : instance.listeners) {
 			ls.onSupplyChange(supply);
 		}
+	}
+
+	@Override
+	public Need[] getNeeds(boolean returnAll) {
+		return null;
+	}
+
+	@Override
+	public boolean fillNeeds(Object offer) {
+		if (offer instanceof Unit) {
+			Unit unit = (Unit) offer;
+			if (unit.getType().isWorker()) {
+				System.out.println("Supplies just received new worker #"+ unit.getID() +"!");
+				UnitType type = Units.Types.SUPPLY.getUnitType();
+				unit.build(SimCities.getLocationForStructure(type, unit), type);
+				return true;
+			}
+		}
+		return false;
 	}
 }
