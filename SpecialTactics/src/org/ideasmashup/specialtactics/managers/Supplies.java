a2ef38b556c5408b3173832ea5922efc93e73549
William ANGER <anger.william@gmail.com>
2014-08-13 21:45:10 +0200

    added temporary locking mechanism to supplies so that a certain amount of the available supply total can be reserved not to be used by others until that amound is "unlocked"
    NOTE: the reserved total is substracted from all calls to Supply.getSupply()
    but you can still get the real value : self.supplyTotal() -  self.supplyUsed()

--- SpecialTactics/src/org/ideasmashup/specialtactics/managers/Supplies.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/managers/Supplies.java
@@ -7,6 +7,7 @@ import org.ideasmashup.specialtactics.agents.Consumer;
 import org.ideasmashup.specialtactics.listeners.SupplyListener;
 import org.ideasmashup.specialtactics.needs.Need;
 import org.ideasmashup.specialtactics.needs.NeedUnit;
+import org.ideasmashup.specialtactics.utils.Utils;
 
 import bwapi.Unit;
 import bwapi.UnitType;
@@ -14,6 +15,10 @@ import bwapi.UnitType;
 public class Supplies implements Consumer {
 
 	protected List<SupplyListener> listeners;
+
+	// FIXME temporary hack to "reserve" resouces (must replace with async
+	//       needs-filling API
+	protected int lockedSupply = 0;
 
 	protected static Supplies instance = null;
 
@@ -29,6 +34,25 @@ public class Supplies implements Consumer {
 		}
 	}
 
+	public static void lockSupply(int amount, boolean lock) {
+		if (lock) {
+			instance.lockedSupply += amount;
+			onSupplyChange(-1);
+		}
+		else {
+			instance.lockedSupply -= amount;
+			onSupplyChange(-1);
+		}
+
+		if (instance.lockedSupply < 0) {
+			System.out.println("ERROR: locked negative supply!! "+ instance.lockedSupply);
+		}
+	}
+
+	public static int getSupply() {
+		return Utils.get().getPlayer().supplyTotal() - Utils.get().getPlayer().supplyUsed() - instance.lockedSupply;
+	}
+
 	public static void addListener(SupplyListener ls) {
 		instance.listeners.add(ls);
 	}
@@ -42,14 +66,20 @@ public class Supplies implements Consumer {
 	}
 
 	public static void onSupplyChange(int supply) {
+		// FIXME implement listeners and locking mechanism like in Resources
+
 		// automatically create supply
-		if (supply <= 3) {
+
+		// FIXME remember when supply is laready being produced so that other workers
+		//       continue being used for mining and other activities
+		if (getSupply() <= 3) {
+			System.out.println("Supply running low, requested next worker for supply building");
 			Needs.add(new NeedUnit(Units.Types.WORKERS.getUnitType(), 0), instance);
 		}
 
 		// call all listeners
 		for (SupplyListener ls : instance.listeners) {
-			ls.onSupplyChange(supply);
+			ls.onSupplyChange(getSupply());
 		}
 	}
 
@@ -65,7 +95,14 @@ public class Supplies implements Consumer {
 			if (unit.getType().isWorker()) {
 				System.out.println("Supplies just received new worker #"+ unit.getID() +"!");
 				UnitType type = Units.Types.SUPPLY.getUnitType();
+
+				// FIXME ugly workaround to prevent minerals and gas for the pylons/dpot
+				//       to be eaten up by other minerals consumers
+				Resources.lockMinerals(type.mineralPrice(), true);
+				Resources.lockGas(type.gasPrice(), true);
+
 				unit.build(SimCities.getLocationForStructure(type, unit), type);
+
 				return true;
 			}
 		}
