e6bf01652e73101dfba5f1fed69df6462740b779
William ANGER <anger.william@gmail.com>
2014-08-14 21:22:43 +0200

    removed unused code from Utils. this class will soon be deprecated

--- SpecialTactics/src/org/ideasmashup/specialtactics/utils/Utils.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/utils/Utils.java
@@ -1,17 +1,7 @@
 package org.ideasmashup.specialtactics.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.ideasmashup.specialtactics.agents.Agent;
-import org.ideasmashup.specialtactics.listeners.UnitListener;
-import org.ideasmashup.specialtactics.needs.Need;
-
 import bwapi.Game;
 import bwapi.Player;
-import bwapi.UnitType;
 
 public class Utils {
 
@@ -20,18 +10,10 @@ public class Utils {
 	private final Player player;
 	private final Game game;
 
-	// TODO split all in distintc maps per categories (units, ressources, etc)
-	private final Map<Need, Agent> needs;
-
-	// map of all unit-specific listeners
-	private final Map<UnitType, ArrayList<UnitListener>> lsUnits;
 
 	protected Utils(Game game) {
 		this.game = game;
 		this.player = game.self();
-
-		this.needs = new HashMap<Need, Agent>();
-		this.lsUnits = new HashMap<UnitType, ArrayList<UnitListener>>();
 	}
 
 	public static Utils init(Game game) {
@@ -53,40 +35,4 @@ public class Utils {
 	public Game getGame() {
 		return game;
 	}
-
-	public void addNeed(Need need, Agent owner) {
-		this.needs.put(need, owner);
-	}
-
-	public void removeNeed(Need need) {
-		this.needs.remove(need);
-	}
-
-	public void removeNeeds(Agent owner) {
-		// remove all occurences with "owner" value
-		// oher syntaxes would only removes the first occurence
-		this.needs.values().removeAll(Collections.singleton(owner));
-	}
-
-	public void addUnitsListener(UnitType type, UnitListener ls) {
-		if (this.lsUnits.containsKey(type)) {
-			this.lsUnits.get(type).add(ls);
-		}
-		else {
-			// need to create a new ArrayList
-			ArrayList<UnitListener> list = new ArrayList<UnitListener>();
-			list.add(ls);
-
-			this.lsUnits.put(type, list);
-		}
-	}
-
-	public boolean removeUnitsListener(UnitType type, UnitListener ls) {
-		if (this.lsUnits.containsKey(type)) {
-			return this.lsUnits.get(type).remove(ls);
-		}
-
-		return false;
-	}
-
 }
