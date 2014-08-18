371386a51d71ede0cc7d1ae946ec61b5f0df02d1
William ANGER <anger.william@gmail.com>
2014-08-18 20:10:07 +0200

    added method to check if agents manager is already storing an  agent

--- SpecialTactics/src/org/ideasmashup/specialtactics/managers/Agents.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/managers/Agents.java
@@ -27,6 +27,10 @@ public class Agents {
 		return instance;
 	}
 
+	public boolean contains(Agent agent) {
+		return agents.contains(agent);
+	}
+
 	public void add(Agent agent) {
 		agents.add(agent);
 	}
