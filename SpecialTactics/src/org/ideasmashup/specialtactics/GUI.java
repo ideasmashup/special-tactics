03977cb91b3a8ca72eaf47c6049f2ebbd5a87e28
William ANGER <anger.william@gmail.com>
2014-08-31 21:53:22 +0200

    replaced previous code by barChart example from giCentre

--- SpecialTactics/src/org/ideasmashup/specialtactics/GUI.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/GUI.java
@@ -8,10 +8,13 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.gicentre.utils.multisketch.EmbeddedSketch;
+import org.gicentre.utils.stat.BarChart;
 import org.ideasmashup.specialtactics.brains.Brain;
 import org.ideasmashup.specialtactics.brains.BrainListener;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class GUI implements BrainListener {
 
@@ -114,73 +117,122 @@ public class GUI implements BrainListener {
 
 	// processing sketch
 
+	public class BlankSketch extends EmbeddedSketch {
+
+		private static final long serialVersionUID = -5577823635847454133L;
+
+		@Override
+		public void setup() {
+			// do nothing...
+		}
+	}
+
 	public class ProcessingApplet extends PApplet {
 
 		private static final long serialVersionUID = 3698978875827336499L;
 
+		// Sketch to show how to construct a slide show containg text, images
+		// and sketches.
+		// Can use the full screen but only if Hansi Raber's 'fullscreen'
+		// library is installed.
+		// Version 1.4, 10th August, 2010.
+		// Author Jo Wood, giCentre.
+
+		// ------------------ Sketch-wide variables ---------------------------
+
+		BarChart barChart;
+		PFont titleFont, smallFont;
+
+		// ------------------ Initialisation ----------------------------------
+
 		@Override
 		public void setup() {
-			size(GUI.WIDTH, GUI.HEIGHT);
-			//background(0);
+			// don't remove try catch otherwise errors not shown!!
+			try {
+				size(GUI.WIDTH, GUI.HEIGHT);
+
+				// /////////////// Setup sketch ///////////////////////////
+				smooth();
+				noLoop();
+
+				titleFont = loadFont("Helvetica-22.vlw");
+				smallFont = loadFont("Helvetica-12.vlw");
+				textFont(smallFont);
+
+				barChart = new BarChart(this);
+				barChart.setData(new float[] { 2462, 2801, 3280, 3983, 4490, 4894, 5642, 6322,
+						6489, 6401, 7657, 9649, 9767, 12167, 15154, 18200, 23124, 28645 });
+				barChart.setBarLabels(new String[] { "1830", "1840", "1850", "1860", "1870",
+						"1880", "1890", "1900", "1910", "1920", "1930", "1940", "1950", "1960",
+						"1970", "1980", "1990", "2000" });
+				barChart.setBarColour(color(200, 80, 80, 100));
+				barChart.setBarGap(2);
+				barChart.setValueFormat("$###,###");
+				barChart.showValueAxis(true);
+				barChart.showCategoryAxis(true);
+				// ///////////////////////////////////////////////////////
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+
+		// ----------------- Processing draw ----------------------------------
 
 		@Override
 		public void draw() {
 			// from: http://www.sebastianoliva.com/en/en/2010/05/using-a-processing-sketch-as-a-java-component/trackback/
 
-			background(0);
-			fill(200);
-			ellipseMode(CENTER);
-			ellipse(mouseX, mouseY, 40, 40);
-
 			// from: http://blog.blprnt.com/blog/blprnt/tutorial-processing-javascript-and-data-visualization
 
-//			float[] numbers = new float[] {
-//				13.4f, 14.5f, 15.0f, 23.2f, 30.9f, 31.3f, 32.9f, 35.1f, 34.3f
-//				};
-//
-//			background(0);
-//			// turn on the lights so that we see shading on the 3D objects
-//			lights();
-//			// move to the center of the sketch before we draw our graph
-//			translate(width / 2, height / 2);
-//			// Tilt about 70 degrees on the X axis - like tilting a frame on the
-//			// wall so that it's on a table
-//			rotateX(1.2f);
-//			// Now, spin around the Z axis as the mouse moves. Like spinning
-//			// that frame on the table around its center
-//			rotateZ(map(mouseX, 0, width, 0, TWO_PI));
-//
-//			for (int i = 0; i < numbers.length; i++) {
-//				// calculate the amount of green in the colour by mapping the
-//				// number to 255 (255 red &amp; 255 green = yellow)
-//				float c = map(numbers[i], min(numbers), max(numbers), 0, 255);
-//				fill(255, c, 0);
-//				// calculate the height of the bar by mapping the number to the
-//				// half the width of the screen minus 50 pixels
-//				float w = map(numbers[i], 0, max(numbers), 0, width / 2 - 50);
-//				// move out 200 pixels from the center
-//				pushMatrix();
-//				translate(200, 0);
-//				box(20, 20, w);
-//				popMatrix();
-//				// after we draw each bar, turn the sketch a bit
-//				rotate(TWO_PI / numbers.length);
-//			}
-
-			// custom using gicentreUtils
-
-//			size(300,200);
-//
-//			BarChart barChart = new BarChart(this);
-//			barChart.setData(new float[] { 0.76f, 0.24f, 0.39f, 0.18f, 0.20f });
-//
-//			// Axis scaling
-//			barChart.setMinValue(0);
-//			barChart.setMaxValue(1);
-//
-//			barChart.showValueAxis(true);
-//			barChart.showCategoryAxis(true);
+			// don't remove try catch otherwise errors not shown!!
+			try {
+				// /////////////// Draw sketch ///////////////////////////
+				background(255);
+				barChart.draw(10, 10, width - 50, height - 50);
+				fill(120);
+				textFont(titleFont);
+				text("Units in production", 70, 30);
+				float textHeight = textAscent();
+				textFont(smallFont);
+				text("Total production per UnitType", 70,
+						30 + textHeight);
+				// ///////////////////////////////////////////////////////
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+
+		// ------------------ Processing keyboard handling --------------------
+
+		@Override
+		public void keyPressed() {
+			if (key == ' ') {
+				// exit on spacebar
+				AI.terminate(0);
+			}
+
+			if (key == CODED) {
+				// Up and down arrows control the speed of animation.
+				if (keyCode == UP) {
+					//
+				}
+				else if (keyCode == DOWN) {
+					//
+				}
+				else if (keyCode == RIGHT) {
+					//
+				}
+				else if (keyCode == LEFT) {
+					//
+				}
+			}
+		}
+
+		// -------------------------- Nested classes --------------------------
+
+		// -------------------------- Private methods -------------------------
+
 	}
 }
