2319df3d38be837a9945b221d23431fed320daaa
William ANGER <anger.william@gmail.com>
2014-08-31 06:49:02 +0200

    integration of demo code using 3D viz in processing with jogl

--- SpecialTactics/src/org/ideasmashup/specialtactics/GUI.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/GUI.java
@@ -130,6 +130,7 @@ public class GUI extends JFrame {
 	}
 
 	public class UpdatesThread extends Thread {
+
 		public UpdatesThread() {
 			//
 		}
@@ -159,10 +160,13 @@ public class GUI extends JFrame {
 
 		private static final long serialVersionUID = 3698978875827336499L;
 
+		private final float[] numbers = new float[] {
+			13.4f, 14.5f, 15.0f, 23.2f, 30.9f, 31.3f, 32.9f, 35.1f, 34.3f
+			};
 
 		@Override
 		public void setup() {
-			size(500, 500);
+			size(500, 500, P3D);
 			background(0);
 		}
 
@@ -170,11 +174,40 @@ public class GUI extends JFrame {
 		public void draw() {
 			// from: http://www.sebastianoliva.com/en/en/2010/05/using-a-processing-sketch-as-a-java-component/trackback/
 
-			background(0);
-			fill(200);
-			ellipseMode(CENTER);
-			ellipse(mouseX, mouseY, 40, 40);
+//			background(0);
+//			fill(200);
+//			ellipseMode(CENTER);
+//			ellipse(mouseX, mouseY, 40, 40);
 
+			// from: http://blog.blprnt.com/blog/blprnt/tutorial-processing-javascript-and-data-visualization
+
+			background(0);
+			// turn on the lights so that we see shading on the 3D objects
+			lights();
+			// move to the center of the sketch before we draw our graph
+			translate(width / 2, height / 2);
+			// Tilt about 70 degrees on the X axis - like tilting a frame on the
+			// wall so that it's on a table
+			rotateX(1.2f);
+			// Now, spin around the Z axis as the mouse moves. Like spinning
+			// that frame on the table around its center
+			rotateZ(map(mouseX, 0, width, 0, TWO_PI));
+
+			for (int i = 0; i < numbers.length; i++) {
+				// calculate the amount of green in the colour by mapping the
+				// number to 255 (255 red &amp; 255 green = yellow)
+				float c = map(numbers[i], min(numbers), max(numbers), 0, 255);
+				fill(255, c, 0);
+				// calculate the height of the bar by mapping the number to the
+				// half the width of the screen minus 50 pixels
+				float w = map(numbers[i], 0, max(numbers), 0, width / 2 - 50);
+				// move out 200 pixels from the center
+				pushMatrix();
+				translate(200, 0);
+				box(20, 20, w);
+				popMatrix();
+				// after we draw each bar, turn the sketch a bit
+				rotate(TWO_PI / numbers.length);
 			}
 		}
 	}
