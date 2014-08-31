174d735b492c4a9bd529799dad4a8e506f14f7df
William ANGER <anger.william@gmail.com>
2014-08-31 07:48:23 +0200

    added new BrainListener interface

---
+++ SpecialTactics/src/org/ideasmashup/specialtactics/brains/BrainListener.java
@@ -0,0 +1,8 @@
+package org.ideasmashup.specialtactics.brains;
+
+public interface BrainListener {
+
+	// TODO put all watchable Brain events in methods so that GUI and other
+	//      entities can react to Brain decisions, data, notifications, etc...
+
+}
