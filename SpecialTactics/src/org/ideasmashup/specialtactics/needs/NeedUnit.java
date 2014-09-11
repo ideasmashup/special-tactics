07d106cb895fbaaea39a185980a08c7fb8f81116
William ANGER <anger.william@gmail.com>
2014-09-11 20:00:45 +0200

    allow unit needs to override their Units.Types value
    because each unit can have more than one "Types", and sometimes it's
    better to be able to force the Type that we want to prioritize

--- SpecialTactics/src/org/ideasmashup/specialtactics/needs/NeedUnit.java
+++ SpecialTactics/src/org/ideasmashup/specialtactics/needs/NeedUnit.java
@@ -2,6 +2,7 @@ package org.ideasmashup.specialtactics.needs;
 
 import org.ideasmashup.specialtactics.agents.Consumer;
 import org.ideasmashup.specialtactics.managers.Needs;
+import org.ideasmashup.specialtactics.managers.Units;
 
 import bwapi.Unit;
 import bwapi.UnitType;
@@ -9,24 +10,36 @@ import bwapi.UnitType;
 public class NeedUnit extends Need {
 
 	protected UnitType unittype;
+	protected Units.Types types;
 
 	public NeedUnit(Consumer owner, UnitType type) {
 		super(owner);
 		this.unittype = type;
+		this.types = Units.Types.getType(type);
 	}
 
 	public NeedUnit(Consumer owner, UnitType type, float priority) {
 		super(owner, priority);
 		this.unittype = type;
+		this.types = Units.Types.getType(type);
 	}
 
 	public NeedUnit(Consumer owner, UnitType type, float priority, Needs.Modifiers modifiers) {
 		super(owner, priority, modifiers);
 		this.unittype = type;
+		this.types = Units.Types.getType(type);
 	}
 
 	public UnitType getUnitType() {
 		return unittype;
+	}
+
+	public Units.Types getTypes() {
+		return types;
+	}
+
+	public void setTypes(Units.Types types) {
+		this.types = types;
 	}
 
 	@Override
