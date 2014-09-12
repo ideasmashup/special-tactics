36114fc9a7fad4f565f2aff5ae8e2c871c79f5fd
William ANGER <anger.william@gmail.com>
2014-09-12 05:19:11 +0200

    new Trail class for storing situationnal data dropped by units on Tiles

---
+++ SpecialTactics/src/org/ideasmashup/specialtactics/tiles/Trail.java
@@ -0,0 +1,39 @@
+package org.ideasmashup.specialtactics.tiles;
+
+import java.util.Date;
+
+public class Trail {
+
+	protected float score;
+	protected Date lastChange;
+
+	public Trail() {
+		this.score = 0;
+		this.lastChange = new Date();
+	}
+
+	public void setScore(float value) {
+		this.score = value;
+		this.lastChange = new Date();
+	}
+
+	public float getScore() {
+		return this.score;
+	}
+
+	public void vent() {
+		// dilute score of this trail by 1%
+		//
+		this.score *= 0.01;
+		this.lastChange = new Date();
+	}
+
+	public void vent(float diluting_factor) {
+		this.score *= diluting_factor;
+		this.lastChange = new Date();
+	}
+
+	public long getAge() {
+		return new Date().getTime() - lastChange.getTime();
+	}
+}
